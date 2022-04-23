0ac0995bed06fb0aec52ad4f6948ef2dc21d7fb6
Vladislav Mazurov <vladdraculavm@gmail.com>
2022-04-23 18:38:04 +0200

    added convertToPlayerMove to Converter

--- Source/Teilaufgabe 2 - Client/GameClient/src/main/java/client/converter/Converter.java
+++ Source/Teilaufgabe 2 - Client/GameClient/src/main/java/client/converter/Converter.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import MessagesBase.MessagesFromClient.EMove;
 import MessagesBase.MessagesFromClient.ETerrain;
 import MessagesBase.MessagesFromClient.HalfMap;
 import MessagesBase.MessagesFromClient.HalfMapNode;
+import MessagesBase.MessagesFromClient.PlayerMove;
 import client.model.Coordinates;
 import client.model.Map;
 import client.model.MapObject;
+import client.model.MovementType;
 import client.model.TerrainType;
 
 public class Converter {
@@ -32,6 +35,20 @@ public class Converter {
 			return ETerrain.Mountain;
 		} else {
 			return ETerrain.Water;
+		}
+	}
+	
+	public PlayerMove convertToPlayerMove(String playerID, MovementType movementType) {
+		if(movementType.equals(MovementType.UP)) {
+			return PlayerMove.of(playerID, EMove.Up);
+		} else if(movementType.equals(MovementType.DOWN)) {
+			return PlayerMove.of(playerID, EMove.Down);
+		} else if(movementType.equals(MovementType.LEFT)) {
+			return PlayerMove.of(playerID, EMove.Left);
+		} else if(movementType.equals(MovementType.RIGHT)) {
+			return PlayerMove.of(playerID, EMove.Right);
+		} else {
+			return new PlayerMove();
 		}
 	}
 }
