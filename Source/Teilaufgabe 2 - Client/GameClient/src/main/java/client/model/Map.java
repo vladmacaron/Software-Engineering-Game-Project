492b742b711afdc16345b305fa947b89d40a7b60
Vladislav Mazurov <vladdraculavm@gmail.com>
2022-04-23 13:31:04 +0200

    added base for Map class

--- Source/Teilaufgabe 2 - Client/GameClient/src/main/java/client/model/Map.java
+++ Source/Teilaufgabe 2 - Client/GameClient/src/main/java/client/model/Map.java
@@ -1,5 +1,45 @@
 package client.model;
 
-public class Map {
+import java.util.HashMap;
+import java.util.Objects;
 
+public class Map {
+	
+	private HashMap<Coordinates, MapObject> mapField;
+
+	public Map(HashMap<Coordinates, MapObject> mapField) {
+		super();
+		this.mapField = mapField;
+	}
+
+	public HashMap<Coordinates, MapObject> getMapField() {
+		return mapField;
+	}
+
+	public void setMapField(HashMap<Coordinates, MapObject> mapField) {
+		this.mapField = mapField;
+	}
+	
+	public void addObjectOnMapField(Coordinates coordinates, ObjectType object) {
+		mapField.get(coordinates).addObjectOnField(object);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(mapField);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Map other = (Map) obj;
+		return Objects.equals(mapField, other.mapField);
+	}
+	
+	
 }
